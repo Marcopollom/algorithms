455ae3bf1274813b51da1d4da65495811b6bfbe3
Marcopollom <marcopollomia@gmail.com>
2020-09-09 14:03:32 -0400

    added factorial.py

---
+++ math/factorial.py
@@ -0,0 +1,37 @@
+"""Python function that finds the last n non-zero digits
+of a factorial of a given number
+"""
+
+def nthLastDigit(n, numb_of_digits): 
+    
+    a = [i for i in range(1, n + 1)]
+    length = len(a)
+
+    countFive = 0
+  
+
+    for i in range(length): 
+        while (a[i] > 0 and a[i] % 5 == 0): 
+            a[i] //= 5
+              
+            countFive += 1
+  
+
+    for i in range(length): 
+        while (countFive and a[i] > 0 and (a[i] & 1) == 0): 
+            a[i] >>= 1
+
+            countFive -= 1
+  
+    ans = 1
+
+    for i in range(length): 
+        ans = (ans * a[i] % 10 ** numb_of_digits) % 10 ** numb_of_digits
+  
+    if (countFive): 
+        ans = (ans * 5) % 10
+  
+    if (ans): 
+        return ans 
+  
+    return -1
