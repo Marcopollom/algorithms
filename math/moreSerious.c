6b1316d7436061b5f92b94571af3039f55ac86bf
Marcopollom <marcopollomia@gmail.com>
2020-11-06 15:59:04 -0500

    Added C Program

---
+++ math/moreSerious.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int* listfy(int n)
+{
+    int* a = malloc(n * 4);
+
+    for (int i = 1; i <= n; i++)
+        a[i-1] = i;
+        
+    return a;
+}
+
+int nthLastDigit(int l, int nthLD)
+{
+    int* lst = listfy(l);
+    
+    int countFive = 0;
+    
+    for(int i = 0; i < l; i++)
+    {
+        while(lst[i] > 0 && lst[i] % 5 == 0)
+        {
+          lst[i] /= 5;
+          countFive++;
+        }
+    }
+    
+    for(int i = 0; i < l; i++)
+    {
+        while((countFive != 0) && (lst[i] > 0) && ((lst[i] & 1) == 0))
+        {
+          lst[i] >>= 1;
+          countFive--;
+        }
+    }
+
+    int ans = 1;
+
+    for(int i = 0; i < l; i++)
+    {
+      ans = (ans * lst[i] % (int) pow(10.0, nthLD)) % (int) pow(10.0, nthLD);
+    }
+
+    if(countFive != 0)
+      ans = (ans * 5) % 10;
+    
+    if(ans != 0)
+      return ans;
+
+    return -1;
+
+}
+
+int main()
+{
+    int a = nthLastDigit(30, 1);
+
+    printf("%d\n", a);
+    
+
+    return 0;
+}
+
